14b10e1a0574a439da0e1b29e9d48238385b6083
ixsluo <xiaoshan.luo@outlook.com>
2020-09-13 17:38:59 +0800

    fix(path): track .pathrc
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

--- scripts/softlink.sh
+++ scripts/softlink.sh
@@ -3,6 +3,7 @@
 ln -sf ~/.dotfiles/system/.bash_profile        ~/.bash_profile
 ln -sf ~/.dotfiles/system/.inputrc             ~/.inputrc
 ln -sf ~/.dotfiles/system/.bashrc              ~/.bashrc
+ln -sf ~/.dotfiles/system/.pathrc              ~/.pathrc
 ln -sf ~/.dotfiles/system/.aliases             ~/.aliases
 ln -sf ~/.dotfiles/app_conf/.vimrc             ~/.vimrc
 ln -sf ~/.dotfiles/app_conf/.czrc              ~/.czrc
